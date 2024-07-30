4a80ee528a574ccc7bc90b60aee1dccdb7f3d196
Ivan Humberto Gonzalez Parra <ihgp2005@gmail.com>
2024-07-30 09:28:12 -0500

    Subiendo el archivo de Java

---
+++ index.js
@@ -0,0 +1,15 @@
+const fn = () => {
+    const miString = "Soy un string!"
+    let b = miString + " Muy feliz"
+    console.log(b)
+}
+
+fn()
+
+const clickeando = () => {
+    let a = "Chanchito feliz" 
+
+    console.log(a)
+
+}
+
